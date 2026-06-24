d18195ac0896f01c43ec525129153f8bc17ca4d1
Karthick385 <karthicknarayanan385@gmail.com>
2026-06-25 00:51:46 +0530

    Fix middleware path case

--- backend/Routes/MatchRoutes.js
+++ backend/Routes/MatchRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require(
-  "../Middleware/authMiddleware"
+  "../middleware/authMiddleware"
 );
 
 const {
